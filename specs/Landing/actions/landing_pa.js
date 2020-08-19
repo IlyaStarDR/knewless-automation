a382984bf9962ea1ff53a1a81e2ffd9de15edfe2
Yurii Ohorodnikov <yurii.ohorodnikov@icloud.com>
2020-08-19 18:16:04 +0300

    add login tests(valid/not valid)

---
+++ specs/Landing/actions/landing_pa.js
@@ -0,0 +1,17 @@
+const LandingPage = require('../page/landing_po')
+const credentials = require('../../../testData.json');
+const page = new LandingPage();
+
+class LandingActions{
+    
+    logIn(){
+        this.waitForLogInButton();
+        page.loginBtn.click();
+    }
+
+    waitForLogInButton(){
+        page.loginBtn.waitForDisplayed(2000);
+    }
+}
+
+module.exports = LandingActions;
